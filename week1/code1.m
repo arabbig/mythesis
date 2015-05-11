437c3ead57815581ef1d419804ba474e1febfb21
Chan Pruksapha <chanp.contact@gmail.com>
2015-05-11 13:39:12 +0100

    Eta & quantile

--- week1/code1.m
+++ week1/code1.m
@@ -1,14 +1,12 @@
-
+trendType = {'poly1', 'exp1'};
+%%
 T = readtable('sp500.csv');
 dates = datenum(table2array(T(:,1)),'dd/mm/yyyy');
 data = T.Index;
 myfts = fints(dates, data);
-
 %%
-str1 = '12/30/1927::12/31/1929';
-str2 = '01/01/2008::12/31/2008';
-fts_window  = myfts(str2);
-
+periods = {'12/30/1927::11/08/1929', '01/01/2007::12/31/2008','05/01/2013::04/30/2015','01/01/2010::12/31/2011'};
+fts_window  = myfts(periods{1});
 
 %%
 y = fts2mat(fts_window.series1);
@@ -21,66 +19,57 @@ x = (0:LEN-1)'/(LEN-1);
 index = y;
 
 %%
-f=fit(x,y,'poly1');
-
-%%
+f=fit(x,y, trendType{2});
 figure
 plot(f, x, y);
 y = y - f(x);
 
 %%
-f=fit(x,y,'exp1');
-
-%%
+f = fit(x,y,'sin2','Upper',[Inf 1 Inf Inf 1 Inf],'Robust','on');
 figure
-plot(f, x, y);
-y = y - f(x);
-
-%%
-f = fit(x, y, 'poly2','Robust','on');
-%%
-figure;
-plot(f, x, y);
-y = y - f(x);
-%%
-f=fit(x, y, 'sin1','Robust','on');
-%%
-figure;
 plot(f, x, y);
 y = y - f(x);
 
 %% 
-%See https://www.kevinsheppard.com/images/9/95/MFE_Toolbox_Documentation.pdf
+% See https://www.kevinsheppard.com/images/9/95/MFE_Toolbox_Documentation.pdf
 
-[trend, cyclic] = bkfilter(y,6,32);
-y = cyclic;
+% [trend, cyclic] = bkfilter(y,6,32);
+% y = cyclic;
 
 %%
 figure
 subplot(3,1,1)
-coefs = cwt(y,1:128,'meyr','plot');
+coefs = cwt(y,1:32,'meyr','plot');
 title('Continuous Transform, absolute coefficients');
 set(gca,'yticklabel',[]);
 ylabel('Scale');
-
 subplot(3,1,2)
-plot(x,index,'b')
+plot(fts_window);
 title('Index')
 
 %%
-coefs_by_day = num2cell(coefs,1);
-coefs_med = cellfun(@median, coefs_by_day);
-coefs_mean = cellfun(@mean, coefs_by_day);
-qfunc = @(q) @(x) quantile(x,q);
-ninethy = qfunc(.90);
-coefs_tail = cellfun(ninethy, coefs_by_day);
-coefs_max = cellfun(@max, coefs_by_day);
-subplot(3,1,3);
-plot(x,coefs_mean,'r');
-hold on
-plot(x,coefs_med,'g');
-hold on
-plot(x,coefs_tail,'b');
-hold on
-plot(x,coefs_max,'c');
 
+coefs_by_day = num2cell(coefs,1);
+morethan = @(tresh) @(x) sum(x>tresh)/length(x);
+eta = morethan(0);
+coefs_eta = cellfun(eta, coefs_by_day);
+hold on
+subplot(3,1,3);
+plot(x,coefs_eta)
+
+%%
+
+pfunc = @(q) @(x) prctile(x,q);
+ninethyP = pfunc(90);
+tenthP = pfunc(10);
+
+tenthP_ccfs = cellfun(tenthP, coefs_by_day);
+median_ccfs = cellfun(@median, coefs_by_day);
+ninethyP_ccfs = cellfun(ninethyP, coefs_by_day);
+
+subplot(3,1,3);
+plot(x, median_ccfs,'r');
+hold on
+plot(x,tenthP_ccfs,'g');
+hold on
+plot(x,ninethyP_ccfs,'b');
